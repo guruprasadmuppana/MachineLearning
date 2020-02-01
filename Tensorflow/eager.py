af5fd1f6a971795f97ed5a366536133c821b8a5c
AI Guru <guruprasadmuppana@gmail.com>
2020-02-01 20:37:41 +0530

    Create eager.py

---
+++ Tensorflow/eager.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 11 15:19:36 2019
+
+@author: Admin
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+import os
+
+import tensorflow as tf
+import tensorflow.contrib.eager as tfe
+tf.enable_eager_execution()
+
+import cProfile
+
+
+
+print(tf.executing_eagerly())
+
+
+
+print(tf.executing_eagerly())
