b72b9bd291220c56bdb3fbe0d68693fb96126817
AI Guru <guruprasadmuppana@gmail.com>
2020-02-19 18:13:29 +0530

    Update sigmod_derivate_plot.py

--- matplotlib/sigmod_derivate_plot.py
+++ matplotlib/sigmod_derivate_plot.py
@@ -14,8 +14,8 @@ def sigmod(x):
     return 1/(1+np.exp(-x))
 
 n=-10
-x = np.linspace(-n,n,1000)
-print( len(x))
+x = np.linspace(-n,n,1000,endpoint=False)
+
 y = sigmod(x)
 plt.plot(x,y, c="r", label="sigmod")
 plt.xlabel("x")
@@ -28,7 +28,7 @@ y_hat = sigmod(x)*(1-sigmod(x))
 
 plt.plot(x,y_hat, c="b", label="derivate of sigmod")
 plt.xlabel("x")
-plt.ylabel("y = sigmod'(x)")
+plt.ylabel("y = sigmod derivate(x)")
 plt.legend()
 plt.show()
 
@@ -38,6 +38,18 @@ plt.xlabel("x")
 plt.ylabel("y")
 plt.legend()
 plt.show()
+
+#y2 = x*(1-x)
+#
+#plt.plot(x,y2, c="y", label="x*(1-x)")
+#plt.grid()
+#plt.xlabel("x")
+#plt.ylabel("y=x*(1-x)")
+#plt.legend()
+#plt.show()
+#print(y[-1])
+#
+
 
 
 
